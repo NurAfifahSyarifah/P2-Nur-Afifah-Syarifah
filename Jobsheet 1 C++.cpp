6990c0edccba069c1aee74cbddd7308089dd8e51
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:16:43 +0700

    Create Jobsheet 1 C++.cpp
    Buatlah program untuk menyelesaikan status cumlaude jika ipk utk cumlaude lebih besar atau sama dengan 3,50

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int ipk = 3.50;
+	
+	if(ipk>=3.50){
+		cout<<" selamat anda cumlaude";
+		
+		}
+	else
+{
+	cout<<"Selamat anda cumlaude";
+}
+	return 0;
+
+}
