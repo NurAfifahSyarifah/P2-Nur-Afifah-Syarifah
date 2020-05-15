28d028f3ce0099588893a4569a07cb9229e4aa63
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:20:49 +0700

    Create Jobsheet 5 C++.cpp
    Buatlah program untuk mendeklarasikan index nilai, dimana diketahui kisaran index nilai A (85-100), B (75-84), C (60-74).

---
+++ Jobsheet 5 C++.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+
+using namespace std;
+int main ()
+{
+int nilai;
+     
+cout<<" Masukan Nilai  : ";
+cin>>nilai;
+if((nilai >= 85)&& (nilai<=100))
+{cout<<"A";
+}
+else if((nilai >= 75)&& (nilai<=84));
+{cout<<"B";
+}
+else if((nilai >= 60)&& (nilai<=74));
+{cout<<"C";
+}
+return 0 ;
+}
