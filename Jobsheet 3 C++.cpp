066816551e04031a23f5b980f06289e3c096f64c
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 21:18:25 +0700

    Create Jobsheet 3 C++.cpp
    Buatlah program untuk membedakan angka ganjil dengan genap

---
+++ Jobsheet 3 C++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+int bilangan;
+
+cout<<"masukan bilangan:";
+cin>>bilangan;
+
+if (bilangan%2==0)
+
+cout<<"bilangan genap"<<endl;
+else
+cout<<"bilangan ganjil"<<endl;
+return 0;
+}
